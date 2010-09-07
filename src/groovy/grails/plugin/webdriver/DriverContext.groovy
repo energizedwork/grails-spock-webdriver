9876006d075b02153dbee83e04a06580bbfbb3d9
Rob Fletcher <rob@energizedwork.com>
2010-09-07 11:17:19 +0100

    simplified driver context

--- src/groovy/grails/plugin/webdriver/DriverContext.groovy
+++ src/groovy/grails/plugin/webdriver/DriverContext.groovy
@@ -1,46 +1,51 @@
 package grails.plugin.webdriver
 
 import grails.build.GrailsBuildListener
-import grails.util.GrailsUtil
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.firefox.FirefoxDriver
-import grails.util.BuildSettingsHolder
-import groovy.transform.Synchronized
+import grails.util.*
 
 class DriverContext implements GrailsBuildListener {
 
-	private static WebDriver driver
+	@Lazy static WebDriver driver = initDriver()
+	@Lazy static ConfigObject config = initConfig()
+	
+	private static boolean closeAfterTest = true
 
-	@Synchronized
-	static WebDriver getDriver() {
-		if (!driver) {
-			driver = driverType.newInstance()
+	static void closeDriver() {
+		if (closeAfterTest) {
+			driver?.close()
 		}
-		driver
 	}
 
-	@Lazy static ConfigObject config = {
+	void receiveGrailsBuildEvent(String name, Object... args) {
+		if (name == "TestPhaseStart" && args[0] == "functional") {
+			closeAfterTest = false
+			driver = driverType.newInstance()
+		} else if (name == "TestPhaseEnd" && args[0] == "functional") {
+			driver?.close()
+		}
+	}
+
+	private static ConfigObject initConfig() {
 		def baseDir = BuildSettingsHolder.settings?.baseDir ?: new File(".")
 		def configFile = new File(baseDir, "grails-app/conf/WebDriverConfig.groovy")
 		if (configFile.isFile()) {
 			println "parsing config from $configFile.absolutePath..."
 			new ConfigSlurper(GrailsUtil.environment).parse(configFile.toURI().toURL())
 		} else {
-			println "no WebDriverConfig.groovy found..."
+			println "no WebDriverConfig.groovy found at $configFile.absolutePath..."
 			null
 		}
-	}()
+	}
 
-	void receiveGrailsBuildEvent(String name, Object... args) {
-		if (name == "TestPhaseStart" && args[0] == "functional") {
-			driver = driverType.newInstance()
-		} else if (name == "TestPhaseEnd" && args[0] == "functional") {
-			driver.close()
-		}
+	private static WebDriver initDriver() {
+		driverType.newInstance()
 	}
 
 	private static Class<? extends WebDriver> getDriverType() {
 		def driverClassName = config?.webdriver?.driver
 		driverClassName ? Class.forName(driverClassName) : FirefoxDriver
 	}
+
 }
